9a9f6ff3f3fed020580da99991c0e45e1d70a5de
kimkyeonghun <kkh115505@naver.com>
2022-08-19 17:41:04 +0900

    save content as dataframe

--- crawl_investing.py
+++ crawl_investing.py
@@ -8,11 +8,13 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
+import pandas as pd
+
 """
-1. 결과물 정리하는 코드 추가해야함
-2. ticker list update
-3. ip 우회?
+1. ticker list update
+2. ip 우회?
 """
+
 
 def get_content(news_href, investing_url):
     articles_href = investing_url+news_href
@@ -20,14 +22,19 @@ def get_content(news_href, investing_url):
     if req.status_code == 200:
         html = req.text
         soup2 = BeautifulSoup(html, 'html.parser')
-        release_time = soup2.find('div',class_='contentSectionDetails').text
-        contents = soup2.find('div',class_='WYSIWYG articlePage').find_all(['p','ul'])
+        release_time = soup2.find(
+            'div', class_='contentSectionDetails').find('span').text
+        contents = soup2.find(
+            'div', class_='WYSIWYG articlePage').find_all(['p', 'ul'])
         text = ''
         for content in contents:
+            if content.text.startswith('By'):
+                continue
             if content.find('a') and "Continue reading" in content.find('a').text:
                 return False, False
-            text += content.text + ' '
+            text += content.text + '\n'
         return text, release_time
+
 
 header = {"User-Agent": "Mozilla/5.0"}
 
@@ -38,14 +45,18 @@ driver = webdriver.Chrome('./chromedriver.exe', options=options)
 
 investing_url = "https://www.investing.com"
 
+data_rows = []
+unique_id = 0
 tickers = ['TSLA']
 for ticker in tickers:
     driver.get(investing_url)
     driver.implicitly_wait(5)
-    search = driver.find_element(by=By.XPATH, value='/html/body/div[4]/header/div[2]/div/div[3]/div[1]/input')
+    search = driver.find_element(
+        by=By.XPATH, value='/html/body/div[5]/header/div[2]/div/div[3]/div[1]/input')
     search.send_keys(ticker)
     driver.implicitly_wait(5)
-    rows = driver.find_element(by=By.XPATH, value= '/html/body/div[4]/header/div[2]/div/div[3]/div[2]/div[1]/div[1]/div[2]/div')
+    rows = driver.find_element(
+        by=By.XPATH, value='/html/body/div[5]/header/div[2]/div/div[3]/div[2]/div[1]/div[1]/div[2]/div')
     for row in rows.find_elements(by=By.TAG_NAME, value='a'):
         if 'USA' in row.find_element(by=By.TAG_NAME, value='i').get_attribute('class'):
             href = row.get_attribute('href')
@@ -57,9 +68,9 @@ for ticker in tickers:
     driver.quit()
     href = href+'-news'+"/"+str(page)
     while True:
-        req = requests.get(href, headers = header)
+        req = requests.get(href, headers=header)
         sleep(0.5)
-        if req.status_code==200:
+        if req.status_code == 200:
             html = req.text
             soup = BeautifulSoup(html, 'html.parser')
             articles = soup.select("#leftColumn > div.mediumTitle1 > article")
@@ -68,24 +79,28 @@ for ticker in tickers:
                 break
 
             for article in articles:
+                unique_id += 1
                 article_id = article['data-id']
-                infos = article.find('div',class_='textDiv')
+                infos = article.find('div', class_='textDiv')
                 news_href = infos.find('a')['href']
-                #Pro version 기사 skip
+                # Pro version 기사 skip
                 if news_href.startswith('/pro'):
                     print(news_href)
                     continue
                 news_title = infos.find('a')['title']
-                provider = infos.find('span').find('span').text
+                provider = infos.find('span').find('span').text.split()[1]
                 sleep(random.random())
-                #외부 사이트 이동하는 provider skip 필요
+                # 외부 사이트 이동하는 provider skip 필요
                 if provider in ['Seeking Alpha']:
                     continue
                 text, release_time = get_content(news_href, investing_url)
                 sleep(random.random())
-            page+=1
+                data_rows.append((unique_id, ticker, provider,
+                                 news_href, release_time, news_title, text))
+            page += 1
             try:
-                next = soup.find('div', class_='sideDiv inlineblock text_align_lang_base_2').find('a')['href']
+                next = soup.find(
+                    'div', class_='sideDiv inlineblock text_align_lang_base_2').find('a')['href']
                 href = investing_url + next
             except:
                 print("No Next")
@@ -94,5 +109,6 @@ for ticker in tickers:
             print("Wrong Connection")
             break
 
-sleep(6)
-driver.quit()
+df = pd.DataFrame(data_rows, columns=[
+                  'article_id', 'Ticker', 'provider', 'url', 'release_time', 'title', 'content'])
+df.to_csv("./results_crawl.csv")
